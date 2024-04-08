aa7c45303646c4fc93be0b680df3004b0ddab632
mathiasgugu <magugu@Mathiass-MacBook-Pro.local>
2024-04-08 17:50:00 +0200

    footer

--- src/components/layout/footer/index.js
+++ src/components/layout/footer/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>index</div>;
+  return (
+    <div className="w-screen h-64  flex items-center justify-center text-xl font-light text-blue-500 border-t ">
+      Javascript Frameworks ca - Mathias
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./header";
 import { Outlet } from "react-router-dom";
+import Footer from "./footer";
 
 const Layout = ({ children }) => {
   return (
     <div>
       <Header />
       <Outlet />
+      <Footer />
     </div>
   );
 };
